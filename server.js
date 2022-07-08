f566bb73905566e46923ee74ccb570c6d4638701
Kimberly West <kdenton993@gmail.com>
2022-07-07 19:35:34 -0600

    started server

---
+++ server.js
@@ -0,0 +1,18 @@
+const { urlencoded } = require('body-parser');
+const express = require ('express');
+const mongoose = require ('mongoose')
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(require('./develop/routes/api'));
+
+ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017')
+
+ mongoose.set('debug', true);
+
+const transaction = require("./develop/models/transaction.js")
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
