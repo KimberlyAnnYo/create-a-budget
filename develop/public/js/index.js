f523a44b08a3289f70965b3ff1c5b447d6c23941
Kimberly West <kdenton993@gmail.com>
2022-07-07 19:47:53 -0600

    working alerts for deposits and expenses

--- develop/public/js/index.js
+++ develop/public/js/index.js
@@ -146,8 +146,10 @@ function sendTransaction(isAdding) {
 
 document.querySelector("#add-btn").onclick = function() {
   sendTransaction(true);
+  alert("You added a deposit!")
 };
 
 document.querySelector("#sub-btn").onclick = function() {
   sendTransaction(false);
+  alert("You have added an expense!")
 };
